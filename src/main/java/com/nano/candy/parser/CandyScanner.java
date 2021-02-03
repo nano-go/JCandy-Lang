39baea7a77841a7231d3e719eea04181ef2ef37c
nano-go <1522862112@qq.com>
2021-02-03 23:52:50 +0800

    feature: add new number literal(0x.., 0b.., 0o.., 0..)

--- src/main/java/com/nano/candy/parser/CandyScanner.java
+++ src/main/java/com/nano/candy/parser/CandyScanner.java
@@ -44,7 +44,7 @@ class CandyScanner implements Scanner {
 	}
 
 	public Token privateNextToken() {
-		loop: while (true) {
+		scanAgain: while (true) {
 			skipWhitespace();
 			
 			char ch = reader.peek();
@@ -59,7 +59,9 @@ class CandyScanner implements Scanner {
 			if (Characters.isDigit(ch)) {
 				this.insertSemi = true;
 				Token tok = readNumberLiteral();
-				if (tok == null) continue loop;
+				if (tok == null) {
+					continue scanAgain;
+				}
 				return tok;
 			}
 			
@@ -83,7 +85,7 @@ class CandyScanner implements Scanner {
 					isInsertSemi = true;
 					literal = readStringLiteral();
 					if (literal == null) {
-						continue loop;
+						continue scanAgain;
 					}
 					kind = TokenKind.STRING;
 					break;
@@ -92,10 +94,10 @@ class CandyScanner implements Scanner {
 					ch = reader.peek();
 					if (ch == '/') {
 						readSingleLineComment();
-						continue loop;
+						continue scanAgain;
 					} else if (ch == '*') {
 						readMultiLineComment();
-						continue loop;
+						continue scanAgain;
 					}
 					kind = switch2('=', TokenKind.DIV_ASSIGN, TokenKind.DIV);
 					break;
@@ -104,7 +106,7 @@ class CandyScanner implements Scanner {
 					ch = reader.peek();
 					if (ch != '&') {
 						reader.error(startPos, "Unknown character: '%c'", ch);
-						continue loop;
+						continue scanAgain;
 					}
 					reader.consume();
 					literal = "&&";
@@ -115,7 +117,7 @@ class CandyScanner implements Scanner {
 					ch = reader.peek();
 					if (ch != '|') {
 						reader.error(startPos, "Unknown character: '%c'", ch);
-						continue loop;
+						continue scanAgain;
 					}
 					reader.consume();
 					literal = "||";
@@ -183,7 +185,7 @@ class CandyScanner implements Scanner {
 					
 				default :
 					reader.error(startPos, "Unknown character: '%c'", ch);
-					continue loop;
+					continue scanAgain;
 			}
 			this.insertSemi = isInsertSemi;
 			return new Token(startPos, literal, kind);
@@ -248,33 +250,132 @@ class CandyScanner implements Scanner {
 		String identifier = reader.savedString();
 		return new Token(startPos, identifier, TokenKind.lookupKind(identifier));
 	}
-
-	private Token readNumberLiteral() {
-		reader.putChar(true);
-		boolean hasRadixPoint = false;	
+	
+	private String baseName(int base) {
+		switch (base) {
+			case 2:
+				return "binary";
+			case 8:
+				return "octal";
+			case 10:
+				return "decimal";
+			case 16:
+				return "hexadecimal";
+		}
+		throw new Error("Unknown base: " + base);
+	}
+	
+	private boolean digits(int base) {
+		boolean hasError = false;
 		for (;;) {
-			if (Characters.isDigit(reader.peek())) {
-				reader.putChar(true);
+			char ch = reader.peek();
+			int point = base;
+			
+			if (ch >= '0' && ch <= '9') {
+				point = ch - '0';
+			} else if ('a' <= Characters.lower(ch) && 
+			           Characters.lower(ch) <= 'f') {
+				point = Characters.lower(ch) - 'a' + 10;
 			} else if (reader.peek() == '_') {
 				reader.consume();
+				continue;
 			} else break;
+			
+			if (point >= base) {
+				reader.error(
+					"Invalid digit '%c' in %s literal.",
+					ch, baseName(base)
+				);
+				hasError = true;	
+			} else {
+				reader.putChar(ch);
+			}
+			reader.consume();
 		}
-		hasRadixPoint = reader.peek() == '.';
-		if (hasRadixPoint) {	
+		return hasError;
+	}
+	
+	private Token readNumberLiteral() {
+		char ch = reader.peek();
+		int base = 10;
+		boolean hasPrefix = false;
+		
+		if (ch == '0') {
 			reader.putChar(true);
-			for (;;) {
-				if (Characters.isDigit(reader.peek())) {
+			switch (reader.peek()) {
+				case 'x': case 'X':
+					hasPrefix = true;
 					reader.putChar(true);
-				} else if (reader.peek() == '_') {
-					reader.consume();
-				} else break;
+					base = 16;
+					break;
+				case 'o': case 'O':
+					hasPrefix = true;
+					reader.putChar(true);
+					base = 8;
+					 break;
+				case 'b': case 'B':
+					hasPrefix = true;
+					reader.putChar(true);
+					base = 2;
+					break;
+				case '.': break;
+				default: base = 8;
+			}
+		} else {
+			reader.putChar(true);
+		}
+		
+		boolean hasError = digits(base);
+		boolean hasRadixPoint = false;	
+		hasRadixPoint = reader.peek() == '.';
+		if (hasRadixPoint) {
+			if (base != 10) {
+				reader.error(
+					"Invalid radix point in %s literal.", 
+					baseName(base)
+				);
+				hasError = true;
+			}
+			reader.putChar(true);
+			hasError = digits(base) ? true : hasError;
+		}
+		
+		if (hasError) {
+			return null;
+		}
+		
+		return toNumberToken(
+			reader.savedString(), 
+			hasPrefix, hasRadixPoint, base
+		);
+	}
+	
+	private Token toNumberToken(String lit, boolean hasPrefix, boolean hasRadixPoint, int base) {
+		String numberLit = lit;
+		if (hasPrefix) {
+			// remove the prefix of a number like 0x...
+			numberLit = lit.substring(2);
+			// format "" will be error. replace it with "0"
+			if (numberLit.length() == 0) {
+				numberLit = "0";
 			}
 		}
-			
-		return new Token(
-			startPos, reader.savedString(), 
-			hasRadixPoint ? TokenKind.DOUBLE : TokenKind.INTEGER
-		);
+		
+		try {
+			if (hasRadixPoint) {
+				double value = Double.valueOf(numberLit);
+				return new Token.DoubleNumberToken(
+					startPos, value, lit
+				);
+			}
+			long value = Long.valueOf(numberLit, base);
+			return new Token.IntegerNumberToken(
+				startPos, value, lit
+			);
+		} catch (NumberFormatException e) {
+			reader.error(startPos, "Invalid number literal '%s'", lit);
+			return null;
+		}
 	}
 	
 	private String readStringLiteral() {
