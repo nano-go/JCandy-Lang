792dd78a0325f8429d61e12134eaa091c7ab6b79
nano-go <1522862112@qq.com>
2021-02-03 23:50:23 +0800

    [test]: add test cases for new number literal(0x.., 0b.., 0o.., 0..)

--- src/test/java/com/nano/candy/parser/ScannerTest.java
+++ src/test/java/com/nano/candy/parser/ScannerTest.java
@@ -9,11 +9,15 @@ import static com.nano.candy.parser.TokenKind.*;
 public class ScannerTest {
 	
 	@Test(timeout = 2000) public void testTokenKinds() {
-		CandyTestCase.run(ScannerTestCase.TK_TEST_CASES) ;
+		CandyTestCase.run(ScannerTestCase.TOK_STREAM_TEST_CASES) ;
+	}
+	
+	@Test(timeout = 2000) public void testNumberLiteral() {
+		CandyTestCase.run(ScannerTestCase.NUMBER_LITERAL_CASES) ;
 	}
 	
 	@Test(timeout = 2000) public void testPositions() {
-		CandyTestCase.run(ScannerTestCase.PE_TEST_CASES) ;
+		CandyTestCase.run(ScannerTestCase.POSITION_AND_ERROR_TEST_CASES) ;
 	}
 	
 }
--- src/test/java/com/nano/candy/parser/ScannerTestCase.java
+++ src/test/java/com/nano/candy/parser/ScannerTestCase.java
@@ -1,6 +1,7 @@
 package com.nano.candy.parser;
 
 import com.nano.candy.common.CandyTestCase;
+import com.nano.candy.common.LoggerMsgChecker;
 import com.nano.candy.config.Config;
 import com.nano.candy.utils.Logger;
 import com.nano.candy.utils.Position;
@@ -10,7 +11,6 @@ import java.util.Arrays;
 
 import static com.nano.candy.parser.TokenKind.*;
 import static org.junit.Assert.*;
-import com.nano.candy.common.LoggerMsgChecker;
 
 public class ScannerTestCase {
 	
@@ -20,27 +20,33 @@ public class ScannerTestCase {
 	
 	static final Logger logger = Logger.getLogger() ;
 	
-	static final CandyTestCase[] TK_TEST_CASES = {
+	public static ArrayList<Token> toTokenList(Scanner scanner) {
+		ArrayList<Token> toks = new ArrayList<>();
+		while (scanner.peek().getKind() != TokenKind.EOF) {
+			toks.add(scanner.peek());
+			scanner.nextToken();
+		}
+		return toks;
+	}
+	
+	/**
+	 * Token stream test case.
+	 *
+	 * Format: Input, Token(TokenKind, [ Literal ])...
+	 */ 
+	protected static final CandyTestCase[] TOK_STREAM_TEST_CASES = {
 		newTKCase("Test__112344__Abc // comment...\n", tk(ID, "Test__112344__Abc"), SEMILN),
 		newTKCase("_abC // comment...", tk(ID, "_abC"), SEMIEOF),
 		newTKCase(" _123bba /** comment...\nabcd*/\n", tk(ID, "_123bba"), SEMILN),
 		newTKCase("_abC /** comment...*/", tk(ID, "_abC"), SEMIEOF),
 		newTKCase("\n\t\t_abc 123 \n", tk(ID, "_abc"), tk(INTEGER, "123"), SEMILN),
-		newTKCase("3.14159", tk(DOUBLE, "3.14159"), SEMIEOF),
-		newTKCase("12345.6789", tk(DOUBLE, "12345.6789"), SEMIEOF),
-		newTKCase("12345_.67_89", tk(DOUBLE, "12345.6789"), SEMIEOF),
-		newTKCase("2345._", tk(DOUBLE, "2345."), SEMIEOF),
-		newTKCase("2_.____", tk(DOUBLE, "2."), SEMIEOF),
-		newTKCase("256", tk(INTEGER, "256"), SEMIEOF),
-		newTKCase("256_", tk(INTEGER, "256"), SEMIEOF),
-		newTKCase("25__________6", tk(INTEGER, "256"), SEMIEOF),
-		newTKCase("_256", tk(ID, "_256"), SEMIEOF),
 		newTKCase("length - 1", tk(ID, "length"), MINUS, tk(INTEGER, "1"), SEMIEOF),
 		newTKCase(
-			"3.14159 * 360 + 180 / 50 % 45", 
-			tk(DOUBLE, "3.14159"), STAR, tk(INTEGER, "360"), PLUS, tk(INTEGER, "180"), 
+			"3.14159 * 0X360+0170 / 50 % 45", 
+			tk(DOUBLE, "3.14159"), STAR, tk(INTEGER, "0X360"), PLUS, tk(INTEGER, "0170"), 
 			DIV, tk(INTEGER, "50"), MOD, tk(INTEGER, "45"),SEMIEOF
 		),
+		newTKCase("0xFFFFFFG", tk(INTEGER, "0xFFFFFF"), tk(ID, "G"), SEMIEOF),
 		newTKCase("a > b \n b <===", tk(ID, "a"), GT, tk(ID, "b"), SEMILN, tk(ID, "b"), LTEQ, EQUAL),
 		newTKCase("a < b \n b >===", tk(ID, "a"), LT, tk(ID, "b"), SEMILN, tk(ID, "b"), GTEQ, EQUAL),
 		newTKCase(
@@ -80,6 +86,7 @@ public class ScannerTestCase {
 		newTKCase("\"abcdefg\\uABCDF\"", tk(STRING, "abcdefg\uABCDF"), SEMIEOF),
 		newTKCase("\"abcdefg\\uabCdF\"", tk(STRING, "abcdefg\uABCDF"), SEMIEOF),
 		newTKCase("\"abcdefg\\uEeeE\"", tk(STRING, "abcdefg\uEEEE"), SEMIEOF),
+		newTKCase("\"abcdefg\\u0000\"", tk(STRING, "abcdefg\u0000"), SEMIEOF),
 		newTKCase("\"abcdefg\\0778\"", tk(STRING, "abcdefg\u003f8"), SEMIEOF),
 		newTKCase("\"abcdefg\\077\"", tk(STRING, "abcdefg\u003f"), SEMIEOF),
 		newTKCase("\"abcdefg\\200\"", tk(STRING, "abcdefg\u0080"), SEMIEOF),
@@ -94,12 +101,12 @@ public class ScannerTestCase {
 		newTKCase("/** what \n what \n what **********/"),
 		newTKCase("// what what what"),
 		newTKCase(""),
-	} ;
+	};
 	
 	private static Token tk(TokenKind tk) {
 		return tk(tk, null) ;
 	}
-	
+
 	private static Token tk(TokenKind tk, String literal) {
 		return new Token(null, literal, tk) ;
 	}
@@ -116,8 +123,83 @@ public class ScannerTestCase {
 		}
 		return new TKindAndLiteralCase(input, tks) ;
 	}
+	
+	/**
+	 * Format: Input, TokenKind, Value
+	 */
+	protected static final CandyTestCase[] NUMBER_LITERAL_CASES = {
+		newNLCase("12345", INTEGER, 12345),
+		newNLCase("0xFFFF", INTEGER, 0xFFFF),
+		newNLCase("0XFFFF", INTEGER, 0xFFFF),
+		newNLCase("0x00ABFF1200EF", INTEGER, 0x00AB_FF_12_00_EFL),
+		newNLCase("01234567", INTEGER, 01234567),
+		newNLCase("0o1234567", INTEGER, 01234567),
+		newNLCase("0O1234567", INTEGER, 01234567),
+		newNLCase("0b000111", INTEGER, 0b000111),
+		newNLCase("0b000101", INTEGER, 0b000101),
+		newNLCase("0B000000", INTEGER, 0b000000),
+		
+		newNLCase("123_456", INTEGER, 123456),
+		newNLCase("1__2__3__", INTEGER, 123),
+		newNLCase("0xCA_FE_BA_BE", INTEGER, 0xCAFEBABEL),
+		newNLCase("0b0000_1001", INTEGER, 0b00001001),
+		newNLCase("3_._14159_265358_9793", DOUBLE, 3.141592653589793D),
+		newNLCase("0._35", DOUBLE, 0.35),
+		
+		newNLCase("0", INTEGER, 0),
+		newNLCase("0b", INTEGER, 0),
+		newNLCase("0o", INTEGER, 0),
+		newNLCase("0x", INTEGER, 0),
+		newNLCase("0.0", DOUBLE, 0),
+	};
 
+	private static NumberLiteralCase newNLCase(String input, TokenKind kind, double value) {
+		return new NumberLiteralCase(input, kind, value);
+	}
+	
+	/**
+	 * Format: Input, hasError, Positions...
+	 * These 'Positions' are where the error is occured 
+	 * if hasError is true.
+	 */
+	static final CandyTestCase[] POSITION_AND_ERROR_TEST_CASES = {
+		newPECase("abc&def|", true, pos(1, 4), pos(1, 8)),
+		newPECase("\n\n abcdefg", false, pos(3, 2), pos(3, 9)),
+		newPECase("\n\t\t_abc 123 \n", false, pos(2, 3), pos(2, 8), pos(2,12)),
+		newPECase("\n//\t\t_abc 123\n\t/*abcd*/abcd/**/", false, pos(3, 10), pos(3, 18)),
+		newPECase("\n\"Oh my god! Are you ok?\"\n\n bbbbb", false, pos(2, 1), pos(2, 25), pos(4, 2), pos(4, 7)),
+		newPECase("/** abc.\n\n\t\t*/≈≈___╳", true, pos(3, 5), pos(3, 6), pos(3, 10)),
+		newPECase("\n\n/** good good", true, pos(3,14)),
+		newPECase("/**abcd", true, pos(1, 8)),
+		newPECase("\"music\nIn the end\"", true, pos(1, 7), pos(2, 12)),
+		newPECase("\"\\uFFFR\"", true, pos(1, 7)),
+		newPECase("\"\\u\"", true, pos(1, 4), pos(1, 5)),
+		newPECase("\"\\888\"", true, pos(1, 3)),
+		newPECase("\"\\788\"", true, pos(1, 4)),
+		newPECase("\"\\777\"", true, pos(1, 5)),
+		newPECase("0bFFFF", true, pos(1, 3), pos(1, 4), pos(1, 5), pos(1, 6)),
+		newPECase("089", true, pos(1, 2), pos(1, 3)),
+		newPECase("01.6", true, pos(1, 3)),
+		newPECase("123456FF", true, pos(1, 7), pos(1, 8)),
+		newPECase("0xFFFFFFFFFFFFFFFFFFFFF", true, pos(1, 1)),
+		newPECase("0B1111111111111111111111111111111111111111111111111111111111111111111111", true, pos(1, 1))
+	} ;
+
+	public static SimulationPositions.Location pos(int line, int col) {
+		return SimulationPositions.loc(line, col) ;
+	}
+
+	public static PositionAndErrorCase newPECase(String input, boolean error, SimulationPositions.Location... locs) {
+		return new PositionAndErrorCase(input, error, locs) ;
+	}
+	
+	
+	
+	/**
+	 * Token Stream Test. Checks kind and literal.
+	 */
 	private static class TKindAndLiteralCase implements CandyTestCase{
+		
 		String input ;
 		Token[] expectedToks ;
 		ArrayList<Token> actualToks ;
@@ -130,15 +212,16 @@ public class ScannerTestCase {
 
 		@Override
 		public void assertCase() {
-			Scanner scanner = ScannerFactory.newScanner("test", input) ;
-
-			LoggerMsgChecker.shouldNotAppearErrors(true, true);
+			Scanner scanner = ScannerFactory.newScanner("token stream", input) ;
 
 			while (scanner.hasNextToken()) {
 				Token tk = scanner.peek() ;
 				actualToks.add(tk(tk.getKind(), tk.getLiteral())) ;
 				scanner.nextToken() ;
 			}
+			
+			LoggerMsgChecker.shouldNotAppearErrors(true, true);
+			
 			assertEquals(msg(), expectedToks.length, actualToks.size()) ;
 			for (int i = 0; i < expectedToks.length; i ++) {
 				Token expected = expectedToks[i] ;
@@ -155,32 +238,57 @@ public class ScannerTestCase {
 		}
 	}
 	
-	static final CandyTestCase[] PE_TEST_CASES = {
-		newPECase("abc&def|", true, pos(1, 4), pos(1, 8)),
+	/**
+	 * Number and Value Test.
+	 */
+	public static class NumberLiteralCase implements CandyTestCase {
+
+		private String literal;
+		private TokenKind kind;
 		
-		newPECase("\n\n abcdefg", false, pos(3, 2), pos(3, 9)),
-		newPECase("\n\t\t_abc 123 \n", false, pos(2, 3), pos(2, 8), pos(2,12)),
-		newPECase("\n//\t\t_abc 123\n\t/*abcd*/abcd/**/", false, pos(3, 10), pos(3, 18)),
-		newPECase("\n\"Oh my god! Are you ok?\"\n\n bbbbb", false, pos(2, 1), pos(2, 25), pos(4, 2), pos(4, 7)),
-		newPECase("/** abc.\n\n\t\t*/≈≈___╳", true, pos(3, 5), pos(3, 6), pos(3, 10)),
-		newPECase("\n\n/** good good", true, pos(3,14)),
-		newPECase("/**abcd", true, pos(1, 8)),
-		newPECase("\"music\nIn the end\"", true, pos(1, 7), pos(2, 12)),
-		newPECase("\"\\uFFFR\"", true, pos(1, 7)),
-		newPECase("\"\\u\"", true, pos(1, 4), pos(1, 5)),
-		newPECase("\"\\888\"", true, pos(1, 3)),
-		newPECase("\"\\788\"", true, pos(1, 4)),
-		newPECase("\"\\777\"", true, pos(1, 5)),
-	} ;
-	
-	public static SimulationPositions.Location pos(int line, int col) {
-		return SimulationPositions.loc(line, col) ;
+		// the type of this value maybe long or double,
+		// and the double value is used here for the comparation,
+		private double value;
+
+		public NumberLiteralCase(String literal, TokenKind kind, double value) {
+			this.literal = literal;
+			this.kind = kind;
+			this.value = value;
+		}
+		
+		@Override
+		public void assertCase() {
+			Scanner scanner = ScannerFactory.newScanner("number literal", literal);
+			
+			Token tok = scanner.peek();
+			LoggerMsgChecker.shouldNotAppearErrors(true, true);
+			
+			// assert the kind and the number value.
+			assertEquals("Input:" + literal, kind, tok.getKind());
+			if (kind == TokenKind.DOUBLE) {
+				assertEquals(
+					"Input:" + literal, 
+					Double.valueOf(value), 
+					Double.valueOf(((Token.DoubleNumberToken) tok).getValue())
+				);
+			} else {
+				assertEquals(
+					"Input:" + literal, 
+					Double.valueOf(value), 
+					Double.valueOf(((Token.IntegerNumberToken) tok).getValue())
+				);
+			}
+			
+			// a test case has only a number literal.
+			assertTrue(
+				scanner.nextToken().getKind() == SEMI
+			);
+		}
 	}
 	
-	public static PositionAndErrorCase newPECase(String input, boolean error, SimulationPositions.Location... locs) {
-		return new PositionAndErrorCase(input, error, locs) ;
-	}
-	
+	/**
+	 * Position and Error Test.
+	 */
 	private static class PositionAndErrorCase implements CandyTestCase {
 
 		SimulationPositions expected ;
@@ -194,7 +302,7 @@ public class ScannerTestCase {
 		
 		@Override
 		public void assertCase() {
-			Scanner scanner = ScannerFactory.newScanner("test.cd", expected.input) ;
+			Scanner scanner = ScannerFactory.newScanner("position and error", expected.input) ;
 			while (scanner.hasNextToken()) {
 				Token tk = scanner.peek() ;
 				if (!hasError) {
@@ -202,6 +310,7 @@ public class ScannerTestCase {
 				}
 				scanner.nextToken() ;
 			}
+			
 			if (hasError) {
 				assertTrue(logger.hadErrors()) ;
 				for (Logger.LogMessage msg : logger.getErrorMessages()) {
