7bc598993cdd59888c38eecddf5358be97903193
nano-go <1522862112@qq.com>
2021-03-03 19:23:50 +0800

    feature: new growCapacity methods.

--- src/main/java/com/nano/candy/utils/ArrayUtils.java
+++ src/main/java/com/nano/candy/utils/ArrayUtils.java
@@ -19,6 +19,34 @@ public class ArrayUtils {
 		return arr;
 	}
 	
+	public static char[] growCapacity(char[] arr, int length) {
+		if (length >= arr.length) {
+			arr = Arrays.copyOf(arr, (int)(arr.length*CAPACITY_FACTOR));
+		}
+		return arr;
+	}
+	
+	public static long[] growCapacity(long[] arr, int length) {
+		if (length >= arr.length) {
+			arr = Arrays.copyOf(arr, (int)(arr.length*CAPACITY_FACTOR));
+		}
+		return arr;
+	}
+	
+	public static short[] growCapacity(short[] arr, int length) {
+		if (length >= arr.length) {
+			arr = Arrays.copyOf(arr, (int)(arr.length*CAPACITY_FACTOR));
+		}
+		return arr;
+	}
+	
+	public static boolean[] growCapacity(boolean[] arr, int length) {
+		if (length >= arr.length) {
+			arr = Arrays.copyOf(arr, (int)(arr.length*CAPACITY_FACTOR));
+		}
+		return arr;
+	}
+	
 	public static <T> T[] growCapacity(T[] arr, int length) {
 		if (length >= arr.length) {
 			arr = Arrays.copyOf(arr, (int)(arr.length*CAPACITY_FACTOR));
