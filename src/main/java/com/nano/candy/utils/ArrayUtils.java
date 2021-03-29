eae929eb6da12f30d323c59c4da37230324ea297
nano-go <1522862112@qq.com>
2021-03-29 15:48:22 +0800

    feature: new methods used to merge arrays.

--- src/main/java/com/nano/candy/utils/ArrayUtils.java
+++ src/main/java/com/nano/candy/utils/ArrayUtils.java
@@ -53,4 +53,60 @@ public class ArrayUtils {
 		}
 		return arr;
 	}
+	
+	public static <T> T[] mergeArray(T[] arr1, T[] arr2) {
+		int oldlen = arr1.length;
+		arr1 = Arrays.copyOf(arr1, arr1.length + arr2.length);
+		System.arraycopy(arr2, 0, arr1, oldlen, arr2.length);
+		return arr1;
+	}
+	
+	public static byte[] mergeArray(byte[] arr1, byte[] arr2) {
+		int oldlen = arr1.length;
+		arr1 = Arrays.copyOf(arr1, arr1.length + arr2.length);
+		System.arraycopy(arr2, 0, arr1, oldlen, arr2.length);
+		return arr1;
+	}
+	
+	public static short[] mergeArray(short[] arr1, short[] arr2) {
+		int oldlen = arr1.length;
+		arr1 = Arrays.copyOf(arr1, arr1.length + arr2.length);
+		System.arraycopy(arr2, 0, arr1, oldlen, arr2.length);
+		return arr1;
+	}
+	
+	public static int[] mergeArray(int[] arr1, int[] arr2) {
+		int oldlen = arr1.length;
+		arr1 = Arrays.copyOf(arr1, arr1.length + arr2.length);
+		System.arraycopy(arr2, 0, arr1, oldlen, arr2.length);
+		return arr1;
+	}
+	
+	public static long[] mergeArray(long[] arr1, long[] arr2) {
+		int oldlen = arr1.length;
+		arr1 = Arrays.copyOf(arr1, arr1.length + arr2.length);
+		System.arraycopy(arr2, 0, arr1, oldlen, arr2.length);
+		return arr1;
+	}
+	
+	public static float[] mergeArray(float[] arr1, float[] arr2) {
+		int oldlen = arr1.length;
+		arr1 = Arrays.copyOf(arr1, arr1.length + arr2.length);
+		System.arraycopy(arr2, 0, arr1, oldlen, arr2.length);
+		return arr1;
+	}
+	
+	public static double[] mergeArray(double[] arr1, double[] arr2) {
+		int oldlen = arr1.length;
+		arr1 = Arrays.copyOf(arr1, arr1.length + arr2.length);
+		System.arraycopy(arr2, 0, arr1, oldlen, arr2.length);
+		return arr1;
+	}
+	
+	public static boolean[] mergeArray(boolean[] arr1, boolean[] arr2) {
+		int oldlen = arr1.length;
+		arr1 = Arrays.copyOf(arr1, arr1.length + arr2.length);
+		System.arraycopy(arr2, 0, arr1, oldlen, arr2.length);
+		return arr1;
+	}
 }
