441eaea2abb14d36e4d3c8bb34a411ca35b9bd69
nano-go <1522862112@qq.com>
2021-04-08 18:01:21 +0800

    feature: the new exception represent failure of tools.

--- src/main/java/com/nano/candy/Main.java
+++ src/main/java/com/nano/candy/Main.java
@@ -1,12 +1,14 @@
 package com.nano.candy;
 import com.nano.candy.main.CandyRun;
+import com.nano.candy.tool.ToolException;
 import com.nano.candy.utils.Options;
 
 public class Main {
 	public static void main(String... args){
 		try {
 			new CandyRun(args).main();
-		}  catch (Options.ParseException | 
+		}  catch (Options.ParseException |
+		          ToolException |
 				  InterruptedException e) {
 			System.err.println(e.getMessage());
 			System.exit(1);
---
+++ src/main/java/com/nano/candy/tool/ToolException.java
@@ -0,0 +1,8 @@
+package com.nano.candy.tool;
+
+public class ToolException extends RuntimeException {
+	
+	public ToolException(String msg) {
+		super(msg);
+	}
+}
