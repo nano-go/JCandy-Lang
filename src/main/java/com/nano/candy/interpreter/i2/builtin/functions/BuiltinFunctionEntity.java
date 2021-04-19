ec23286405e6aadddb7bc7aad2dc28eba397c3ba
nano-go <1522862112@qq.com>
2021-04-19 23:53:24 +0800

    refactor: remove the individual class of the built-in function.

--- src/main/java/com/nano/candy/interpreter/i2/builtin/functions/BuiltinFunctionEntity.java
+++ src/main/java/com/nano/candy/interpreter/i2/builtin/functions/BuiltinFunctionEntity.java
@@ -1,16 +1,9 @@
 package com.nano.candy.interpreter.i2.builtin.functions;
 
-import com.nano.candy.interpreter.i2.builtin.annotation.BuiltinClass;
 import com.nano.candy.interpreter.i2.builtin.type.CallableObj;
-import com.nano.candy.interpreter.i2.builtin.type.classes.BuiltinClassFactory;
-import com.nano.candy.interpreter.i2.builtin.type.classes.CandyClass;
 import com.nano.candy.interpreter.i2.vm.VM;
 
-@BuiltinClass("BuiltinFunction")
 public class BuiltinFunctionEntity extends CallableObj {
-	
-	public static final CandyClass BUILTIN_FUNCTION_CLASS =
-		BuiltinClassFactory.generate(BuiltinFunctionEntity.class, CALLABLE_CLASS);
 	
 	Callback callback;
 
