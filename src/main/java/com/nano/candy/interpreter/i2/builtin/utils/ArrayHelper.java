d485f131d16aa7d902b93491d3a597dd444b929d
nano-go <1522862112@qq.com>
2021-06-21 21:40:03 +0800

    feature: ArrayHelper provides helpful methods used to convert a basic type array into a candy array.

--- src/main/java/com/nano/candy/interpreter/i2/builtin/utils/ArrayHelper.java
+++ src/main/java/com/nano/candy/interpreter/i2/builtin/utils/ArrayHelper.java
@@ -1,6 +1,10 @@
 package com.nano.candy.interpreter.i2.builtin.utils;
 
 import com.nano.candy.interpreter.i2.builtin.CandyObject;
+import com.nano.candy.interpreter.i2.builtin.type.ArrayObj;
+import com.nano.candy.interpreter.i2.builtin.type.BoolObj;
+import com.nano.candy.interpreter.i2.builtin.type.DoubleObj;
+import com.nano.candy.interpreter.i2.builtin.type.IntegerObj;
 import com.nano.candy.interpreter.i2.builtin.type.StringObj;
 import com.nano.candy.interpreter.i2.vm.VM;
 
@@ -35,5 +39,69 @@ public class ArrayHelper {
 			}
 		}
 		return str.toString();
+	}
+	
+	public static ArrayObj toCandyStringArr(String[] arr) {
+		ArrayObj candyArr = new ArrayObj(arr.length);
+		for (String e : arr) {
+			candyArr.append(StringObj.valueOf(e));
+		}
+		return candyArr;
+	}
+	
+	public static ArrayObj toCandyIntegerArr(long[] arr) {
+		ArrayObj candyArr = new ArrayObj(arr.length);
+		for (long e : arr) {
+			candyArr.append(IntegerObj.valueOf(e));
+		}
+		return candyArr;
+	}
+
+	public static ArrayObj toCandyIntegerArr(int[] arr) {
+		ArrayObj candyArr = new ArrayObj(arr.length);
+		for (int e : arr) {
+			candyArr.append(IntegerObj.valueOf(e));
+		}
+		return candyArr;
+	}
+
+	public static ArrayObj toCandyIntegerArr(short[] arr) {
+		ArrayObj candyArr = new ArrayObj(arr.length);
+		for (short e : arr) {
+			candyArr.append(IntegerObj.valueOf(e));
+		}
+		return candyArr;
+	}
+
+	public static ArrayObj toCandyIntegerArr(byte[] arr) {
+		ArrayObj candyArr = new ArrayObj(arr.length);
+		for (byte e : arr) {
+			candyArr.append(IntegerObj.valueOf(e));
+		}
+		return candyArr;
+	}
+
+	public static ArrayObj toCandyDoubleArr(double[] arr) {
+		ArrayObj candyArr = new ArrayObj(arr.length);
+		for (double e : arr) {
+			candyArr.append(DoubleObj.valueOf(e));
+		}
+		return candyArr;
+	}
+	
+	public static ArrayObj toCandyDoubleArr(float[] arr) {
+		ArrayObj candyArr = new ArrayObj(arr.length);
+		for (double e : arr) {
+			candyArr.append(DoubleObj.valueOf(e));
+		}
+		return candyArr;
+	}
+	
+	public static ArrayObj toCandyBooleanArr(boolean[] arr) {
+		ArrayObj candyArr = new ArrayObj(arr.length);
+		for (boolean e : arr) {
+			candyArr.append(BoolObj.valueOf(e));
+		}
+		return candyArr;
 	}
 }
