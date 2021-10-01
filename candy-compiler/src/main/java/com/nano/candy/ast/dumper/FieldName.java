7883b7abcf924c9979aad88997e4e27dec48f148
nano-go <1522862112@qq.com>
2021-10-01 22:34:02 +0800

    docs: details.

--- candy-compiler/src/main/java/com/nano/candy/ast/dumper/FieldName.java
+++ candy-compiler/src/main/java/com/nano/candy/ast/dumper/FieldName.java
@@ -5,6 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Fields in the AST node which are annotated with this will be renamed
+ * to the {@code value()}.
+ *
+ * <p> This is parsed by {@code SerializableDumper}.
+ */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface FieldName {
--- candy-compiler/src/main/java/com/nano/candy/ast/dumper/NodeName.java
+++ candy-compiler/src/main/java/com/nano/candy/ast/dumper/NodeName.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * AST nodes annotated with this will be renamed to the {@code value()}.
+ *
+ * <p> This is parsed by {@code SerializableDumper}.
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface NodeName {
